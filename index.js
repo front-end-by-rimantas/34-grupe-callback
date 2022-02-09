f9e6952765549782db1df9499ecef79625f1f0d6
Rimantas <belauzas@yahoo.com>
2022-02-09 18:44:55 +0200

    Math

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('eresthj');
+const matematika = {};
+
+matematika.plius = (a, b) => a + b;
+matematika.minus = (a, b) => a - b;
+matematika.padalinti = (a, b) => a / b;
+matematika.padauginti = (a, b) => a * b;
+
+const pirmasSkaicius = 7;
+const antrasSkaicius = 5;
+const veiksmas = 'plius';
+
+const veiksmoFunkcija = matematika[veiksmas];
+
+function skaiciuojam(a, operation, b) {
+    return operation(a, b);
+}
+
+const rezultatas = skaiciuojam(pirmasSkaicius, veiksmoFunkcija, antrasSkaicius);
+
+console.log(rezultatas);
